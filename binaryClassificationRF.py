26ad9192ba126e092bfe4c4216f8c868f96c6cda
tsanko <tsanko_hr@yahoo.com>
2017-03-16 12:21:00 +0100

    binary classif modified

--- binaryClassificationRF.py
+++ binaryClassificationRF.py
@@ -1,1 +1,73 @@
-test
+import urllib2
+from math import sqrt, fabs, exp
+import matplotlib.pyplot as plot
+from sklearn.cross_validation import train_test_split
+from sklearn import ensemble
+from sklearn.metrics import roc_auc_score, roc_curve, accuracy_score
+import numpy
+
+#read data from uci data repository
+target_url = ("https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data")
+data = urllib2.urlopen(target_url)
+
+#arrange data into list for labels and list of lists for attributes
+xList = []
+
+for line in data:
+    #split on comma
+    row = line.strip().split(",")
+    xList.append(row)
+
+#separate labels from attributes, convert from attributes from
+#string to numeric and convert "M" to 1 and "R" to 0
+
+xNum = []
+labels = []
+
+for row in xList:
+    lastCol = row.pop()
+    if lastCol == "M":
+        labels.append(1)
+    else:
+        labels.append(0)
+    attrRow = [float(elt) for elt in row]
+    xNum.append(attrRow)
+
+#number of rows and columns in x matrix
+nrows = len(xNum)
+ncols = len(xNum[1])
+
+#form x and y into numpy arrays and make up column names
+X = numpy.array(xNum)
+y = numpy.array(labels)
+rocksVMinesNames = numpy.array(['V' + str(i) for i in range(ncols)])
+
+#break into training and test sets.
+xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size=0.30,
+    random_state=531)
+
+auc = []
+nTreeList = [1000]
+misclassErrors = []
+
+for iTrees in nTreeList:
+    depth = None
+    maxFeat  = None
+    rocksVMinesRFModel = ensemble.RandomForestClassifier(n_estimators=
+                             iTrees, max_depth=depth, max_features=
+                             maxFeat, oob_score=False, random_state=531, n_jobs=-1)
+    rocksVMinesRFModel.fit(xTrain,yTrain)
+
+    #Accumulate auc on test set
+    prediction = rocksVMinesRFModel.predict(xTest)
+
+    
+    correct = accuracy_score(yTest, prediction)
+
+    misclassErrors.append(1.0 - correct)
+
+
+
+print("misclassError" )
+print(misclassErrors[-1])
+
